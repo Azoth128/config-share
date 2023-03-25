568dcec0316818fa02dee6d0420801e3fd287ac5
Tim Wundenberg <tim.wundenberg@breuninger.de>
2023-03-25 21:18:54 +0100

    add list of files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+
+#[tokio::main]
+async fn main() -> () {
+    let files_to_encrypt = get_files();
+
+    print!("{:?}", files_to_encrypt);
+}
+
+fn get_files() -> Vec<String> {
+    fs::read_dir(".")
+        .unwrap()
+        .map(|entry| {
+            entry.unwrap().file_name().to_str().map(|e| String::from(e))
+        })
+        .filter_map(|e| e)
+        .collect()
 }
