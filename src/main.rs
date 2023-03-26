17d4ff0dc18e2ef10e817c4f08c70a775e317f72
Tim Wundenberg <tim.wundenberg@breuninger.de>
2023-03-26 13:27:39 +0200

    list files with subdirs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
-use std::fs;
+use std::fs::{self, DirEntry};
 
 #[tokio::main]
 async fn main() -> () {
-    let files_to_encrypt = get_files();
+    let files_to_encrypt = get_files(".");
 
-    print!("{:?}", files_to_encrypt);
+    for file in files_to_encrypt {
+        println!("{}", file);
+    }
 }
 
-fn get_files() -> Vec<String> {
-    fs::read_dir(".")
+fn with_files_in_dirs(entry: DirEntry) -> Vec<String> {
+    let file_type = entry.file_type().unwrap();
+    let file_name = match entry.path().to_str().map(|e| String::from(e)) {
+        Some(str) => str,
+        None => return vec![],
+    };
+
+    if file_type.is_file() {
+        vec![file_name]
+    } else if file_type.is_dir() {
+        get_files(&file_name)
+    } else {
+        vec![]
+    }
+}
+
+fn get_files(path: &str) -> Vec<String> {
+    fs::read_dir(path)
         .unwrap()
-        .map(|entry| {
-            entry.unwrap().file_name().to_str().map(|e| String::from(e))
-        })
-        .filter_map(|e| e)
+        .map(|entry| entry.unwrap())
+        .flat_map(|entry| with_files_in_dirs(entry))
         .collect()
 }
